be3c02801b4be9933e41db6214fdcfd448f78676
GuaKing <55473221+GuaKing@users.noreply.github.com>
2020-11-10 10:49:11 +0800

    add get value function to bugzilla_pak.py

--- Tool/Bugzilla_Proc/Bugzilla_Pak.py
+++ Tool/Bugzilla_Proc/Bugzilla_Pak.py
@@ -14,3 +14,20 @@ def setValue(br,url,di):
         formLogin.set(d, di[d])
     #br.submit_selected()
     #br.launch_browser()
+
+def getOptionValue(br,url,Li):
+    br.open(url)
+    loginform = br.select_form(nr=1)
+    #select = loginform.form.find("select", {"name": keyv})
+    #option = select.find("option", {"selected": "selected"})
+    #return (option["value"])
+
+    Di={}
+    for i in range(len(Li)):
+        select = loginform.form.find("select", {"name": Li[i]})
+        option = select.find("option", {"selected": "selected"})
+        Di['{}'.format(Li[i])] = option["value"]
+    #br.launch_browser()
+
+    return Di
+
